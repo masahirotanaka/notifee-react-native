a605166b09edddf65a6dfbb76ccee2a273b27fdd
Masahiro Tanaka <masahiro@asial.co.jp>
2024-04-26 19:09:24 +0200

    Initial

---
+++ example/android/app/src/main/jni/MainApplicationModuleProvider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <ReactCommon/JavaTurboModule.h>
+
+#include <memory>
+#include <string>
+
+namespace facebook {
+namespace react {
+
+std::shared_ptr<TurboModule> MainApplicationModuleProvider(
+    const std::string &moduleName, const JavaTurboModule::InitParams &params);
+
+}  // namespace react
+}  // namespace facebook
